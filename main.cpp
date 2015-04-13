a406df3efbce855c265230dadfa9582f0a8947d3
Gregg <gmcmillion215@gmail.com>
2015-04-13 11:17:58 -0700

    testing

--- main.cpp
+++ main.cpp
@@ -1,17 +1,40 @@
+//Pokedex
 #include <iostream>
-#include "Items.h"
 using namespace std;
 
-int main() {
-    Items one;
-    one.addItem("Potion");
-    one.addItem("Potion");
-    one.addItem("Potion");
-    one.addItem("Super Postion");
-    one.addItem("Feather");
-    one.addItem("A large book");
-    one.displayAll();
+int main()
+{
+    char choice;
     
-    one.useItem(2);
-    one.displayAll();
+    //Display Menu
+    cout << "\t\tPokemon Menu: \n";
+    cout << "(A) Pokedex \n";
+    cout << "(B) Pokemon \n";
+    cout << "(C) Items \n";
+    cout << "(D) Badges \n";
+    cout << "(D) Exit \n";
+    cout << "Enter choice A, B, C, D, or E: ";
+    
+    cin >> choice;
+    
+    switch (choice)
+    {
+        case 'a':
+        case 'A': cout << "You entered Pokedex. \n";
+                  break;
+        case 'b':
+        case 'B': cout << "You entered Pokemon. \n";
+                  break;
+        case 'c':
+        case 'C': cout << "You entered Items. \n";
+                  break;
+        case 'd':
+        case 'D': cout << "You entered Badges. \n";
+                  break;
+        case 'e':
+        case 'E': cout << "Goodbye! \n";
+                  break;
+        default:  cout << "Invalid, please enter A, B, C, D, or E!\n";
+    }
+    return 0;
 }
